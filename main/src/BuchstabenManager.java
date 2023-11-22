43b437003cb21e566b2bc9136658084d02e95d77
Jochen Huber <jochen.huber@stud.fh-campuswien.ac.at>
2023-11-22 13:54:18 +0100

    Neuer Ansatz Buchstabenmanager (Jochen)

--- main/src/BuchstabenManager.java
+++ main/src/BuchstabenManager.java
@@ -5,26 +5,21 @@ public class BuchstabenManager {
     public char buchstabenVergleich() {
     String loesung = "Bauer";
     String eingabe = "Alter";
-    if (eingabe.equals(loesung)) {
-        System.out.println("You won!");
-    }
-
-
-    //char[] eingabeFolge = eingabe.toCharArray();
-    //char[] loesungFolge = loesung.toCharArray();
+    int[] anzeigeArray = {0, 0, 0, 0, 0};                   // 0 = Zeichen falsch; 1 = Zeichen rot (vorhanden aber falsche Stelle), 2 = Zeichen richtig
 
     if (eingabe.equals(loesung)) {
         System.out.println("You Won!");
     } else
         for (int i = 0; i <= loesung.length(); i++) {
-            if (eingabe.charAt(i) == (loesung.charAt(i))) {
-                return;                                         // Zeichen ist gleich
+            if (eingabe.charAt(i) == loesung.charAt(i)) {       // Zeichen ist korrekt => anzeigeArray = 2
+                anzeigeArray[i] = 2;
+            }
+            else if (eingabe.charAt(i) == loesung.charAt()) {   // Zeichen ist vorhanden aber falsche Stelle => anzeigeArray = 1
+                anzeigeArray[i] = 1;
+            }
+            else {                                              // Zeichen falsch => anzeigeArray
+                anzeigeArray[i] = 0;
             }
         }
     }
-
-
-
-
-
 }
