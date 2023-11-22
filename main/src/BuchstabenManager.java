4a5f3c355f605e87fc14e643bf8c7f9ae2045d00
Jochen Huber <jochen.huber@stud.fh-campuswien.ac.at>
2023-11-22 15:10:40 +0100

    Main: provisorische Worteingabe eingefügt BuchstabenManager: kann zwei Begriffe zeichenweise vergleichen, gibt eine Array retour um die entsprechenden Lösungs-Zeichen zu kodieren (siehe Kommentar)

--- main/src/BuchstabenManager.java
+++ main/src/BuchstabenManager.java
@@ -1,25 +1,27 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class BuchstabenManager {
 
-    public char buchstabenVergleich() {
-    String loesung = "Bauer";
-    String eingabe = "Alter";
-    int[] anzeigeArray = {0, 0, 0, 0, 0};                   // 0 = Zeichen falsch; 1 = Zeichen rot (vorhanden aber falsche Stelle), 2 = Zeichen richtig
+    public static int[] buchstabenVergleich(String loesung, String eingabe) {
+        int[] anzeigeArray = {0, 0, 0, 0, 0};                           // 0 = Zeichen falsch; 1 = Zeichen rot (vorhanden aber falsche Stelle), 2 = Zeichen richtig
 
-    if (eingabe.equals(loesung)) {
-        System.out.println("You Won!");
-    } else
-        for (int i = 0; i <= loesung.length(); i++) {
-            if (eingabe.charAt(i) == loesung.charAt(i)) {       // Zeichen ist korrekt => anzeigeArray = 2
-                anzeigeArray[i] = 2;
-            }
-            else if (eingabe.charAt(i) == loesung.charAt()) {   // Zeichen ist vorhanden aber falsche Stelle => anzeigeArray = 1
-                anzeigeArray[i] = 1;
-            }
-            else {                                              // Zeichen falsch => anzeigeArray
-                anzeigeArray[i] = 0;
+        if (eingabe.equals(loesung)) {
+            System.out.println("You Won!");
+        } else {
+            for (int i = 0; i < loesung.length(); i++) {
+                if (eingabe.charAt(i) == loesung.charAt(i)) {           // Zeichen ist korrekt => anzeigeArray[i] = 2
+                    anzeigeArray[i] = 2;
+                } else {
+                    for (int j = 0; j < loesung.length(); j++) {
+                        if (eingabe.charAt(i) == loesung.charAt(j)) {   // Zeichen ist vorhanden aber falschen Stelle => anzeigeArray[i] = 1
+                            anzeigeArray[i] = 1;
+                        }
+                    }
+                }                                                       // sonst bleibt anzeigeArray[i] = 0;
             }
         }
+        System.out.println(Arrays.toString(anzeigeArray));
+        return anzeigeArray;
     }
+
 }
--- main/src/Main.java
+++ main/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Willkommen bei Wordle!");
+
+        // provisorische Worteingabe
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Geben Sie das Lösungswort ein: ");
+        String loesung = scanner.nextLine();
+        System.out.print("Geben Sie das Eingabewort ein): ");
+        String eingabe = scanner.nextLine();
+
+
+        //
+        BuchstabenManager.buchstabenVergleich(loesung, eingabe);
     }
 }
